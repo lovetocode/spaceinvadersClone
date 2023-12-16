a574c92c1e4f92b0c25fec8e6c661e15b57c5591
Daniel Thornton <dshaggy28@yahoo.com>
2023-12-15 17:11:55 -0800

    Added the player to main scene.

--- scripts/player.gd
+++ scripts/player.gd
@@ -1,12 +1,20 @@
 extends Area2D
 
-@export var speed = 20
+@export var speed = 200
 var direction = Vector2.ZERO
 
 @onready var collision_shape_2d = $CollisionShape2D
 
+var bounding_size_x
+var start_bound
+var end_bound
+
 func _ready():
-	pass # Replace with function body.
+	bounding_size_x = collision_shape_2d.shape.get_rect().size.x
+	var rect = get_viewport().get_visible_rect()
+	var camera = get_viewport().get_camera_2d()
+	var camera_position = camera.position
+	
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -20,3 +28,7 @@ func _process(delta):
 		direction = Vector2.LEFT
 	else:
 		direction = Vector2.ZERO
+		
+	var delta_movement = delta * speed * direction.x
+	
+	position.x += delta_movement
